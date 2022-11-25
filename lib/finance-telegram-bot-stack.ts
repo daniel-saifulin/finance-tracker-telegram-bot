72242773efd909d4717d2594091609e214bbdd89
Daniel Saifulin <daniel.saifulin@flatstack.com>
2022-11-25 21:19:47 +0100

    Initial commit

---
+++ lib/finance-telegram-bot-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class FinanceTelegramBotStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'FinanceTelegramBotQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
